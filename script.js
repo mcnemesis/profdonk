05250fc7785fdfc753b86edd1f269708068f6fd0
JWL <joewillirich@gmail.com>
2025-01-29 15:12:21 +0300

    style tag cloud section better

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ $(document).ready(function () {
 
 
 function renderTagCloud(entries){
-  var tagVw = window.innerWidth * 0.38;
+  var tagVw = window.innerWidth * 0.68;
    var settings = {
                 entries: entries,
                 width: tagVw,
